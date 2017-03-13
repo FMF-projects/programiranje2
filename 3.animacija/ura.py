760d7cdbd4642e969994469f8b5a1ffe260ef607
Klementina Pirc <klementina.pirc@student.fmf.uni-lj.si>
2017-03-13 23:00:02 +0100

    deeelaaa :D

--- 3.animacija/ura.py
+++ 3.animacija/ura.py
@@ -5,8 +5,6 @@ from datetime import *
 class Ura():
 
     def __init__(self, master):
-    
-        self.zacetni_cas = datetime.now()
 
         self.platno = Canvas(master, width=300, height=300)
         self.platno.grid(row=0, column=0)
@@ -14,13 +12,12 @@ class Ura():
         self.platno.create_oval(10, 10, 290, 290)
 
         #vstavimo številke
-        kot = 0
-        stevila = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2]
-        for i in stevila:
-            x = 130 * cos(radians(kot))
-            y = 130 * sin(radians(kot))
+        kot = -pi/3
+        for i in range(1, 13):
+            x = 130 * cos(kot)
+            y = 130 * sin(kot)
             self.platno.create_text(150 + x, 150 + y, text=str(i))
-            kot += 30
+            kot += pi/6
 
         #vstavimo kazalce
         self.sek = self.platno.create_line(150, 150, 150, 10)
@@ -33,17 +30,26 @@ class Ura():
         
     def premakni(self):
         '''premakne kazalec na nov položaj'''
-        self.kot += 6
-        x = 150 + 140 * cos(radians(self.kot))
-        y = 150 + 140 * sin(radians(self.kot))
-        #sekunde = self.zacetni_cas.second()
-        #trenutni = datetime.now()
-        #if trenutni.second() - sekunde == 1:
-        self.platno.coords(self.sek, 150, 150, x, y)
+        zdaj = datetime.now()
+        ura = zdaj.hour
+        minuta = zdaj.minute
+        sekunda = zdaj.second
         
-        self.platno.after(1000, self.premakni)        
+        x_s = 150 + 140 * cos((sekunda / 60) * 2 * pi - pi/2)
+        y_s = 150 + 140 * sin((sekunda / 60) * 2 * pi - pi/2)
+        
+        x_m = 150 + 110 * cos((minuta/ 60) * 2 * pi - pi/2)
+        y_m = 150 + 110 * sin((minuta/ 60) * 2 * pi - pi/2)
+        
+        x_u = 150 + 70 * cos((ura/ 12) * 2 * pi - pi/2)
+        y_u = 150 + 70 * sin((ura/ 12) * 2 * pi - pi/2)
+        
+        self.platno.coords(self.sek, 150, 150, x_s, y_s)
+        self.platno.coords(self.min, 150, 150, x_m, y_m)
+        self.platno.coords(self.ura, 150, 150, x_u, y_u)
+        
+        self.platno.after(200, self.premakni)        
 
-
-root = Tk()
+root = Tk()        
 aplikacija = Ura(root)
 root.mainloop()
