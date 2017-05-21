420e0b9cf7a29872bcccc38b3a609a1db51e37be
Klementina Pirc <klementina.pirc@student.fmf.uni-lj.si>
2017-05-21 18:34:01 +0200

    frekvence besed - vektor

--- 6.datoteke,OOP/src/Frekvence_besed.java
+++ 6.datoteke,OOP/src/Frekvence_besed.java
@@ -4,11 +4,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*; //* da ne dodaja vsakega .util posebej
 
+import javafx.util.Pair;
+
 public class Frekvence_besed {
 
 	public static void main(String[] args) throws IOException {
-		Set<String> spreglej_besede = ne_stej_besed("spreglej.txt");
-		System.out.println(pojavitve_besed("Hisa_Marije_pomocnice.txt", spreglej_besede));
+		System.out.println(frekvence_besed("Hisa_Marije_pomocnice.txt", "spreglej.txt"));
 
 	}
 
@@ -46,5 +47,37 @@ public class Frekvence_besed {
 		}
 		return besede;
 	}
+	
+	public static Vector<String> vektor(Map<String, Integer> slovar){
+		Vector<String> besede = new Vector();
+		int max = 0;
+		for (String beseda : slovar.keySet()){
+			int vrednost = slovar.get(beseda);
+			if (vrednost > max){
+				max = vrednost;
+			}
+		}
+		for (int i=max; i >= 1; i--){
+			for (String b : slovar.keySet()){
+				if (slovar.get(b) == i){
+					besede.add(b);
+				}
+			}
+		}
+		return besede;
+	}
+	
+	public static List<String> frekvence_besed(String datoteka, String spreglej) throws IOException{
+		Set<String> spreglej_besede = ne_stej_besed(spreglej);
+		Map<String, Integer> slovar_besed = pojavitve_besed("Hisa_Marije_pomocnice.txt", spreglej_besede);
+		Vector<String> vektor_besed = vektor(slovar_besed);
+		
+		List<String> besede = new ArrayList<String>();
+		for (String beseda : vektor_besed){
+			besede.add("(" + beseda + ":" + String.valueOf(slovar_besed.get(beseda)) + ")" );
+		}
+		return besede;
+	}
 }
+
 
